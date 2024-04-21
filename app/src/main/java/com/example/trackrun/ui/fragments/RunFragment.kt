a59ca0171b21672f7131f5420af369ee9c4cc5b1
Devansh <devanshgaur75@gmail.com>
2024-04-21 23:58:39 +0530

    Setup Navigation

--- app/src/main/java/com/example/trackrun/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/example/trackrun/ui/fragments/RunFragment.kt
@@ -1,8 +1,14 @@
 package com.example.trackrun.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.trackrun.R
+import com.example.trackrun.databinding.FragmentRunBinding
 import com.example.trackrun.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,4 +16,21 @@ import dagger.hilt.android.AndroidEntryPoint
 class RunFragment : Fragment(R.layout.fragment_run) {
 
     private val viewModel: MainViewModel by viewModels()
+    private lateinit var binding: FragmentRunBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentRunBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvRun.setOnClickListener {
+            findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
+        }
+    }
 }
